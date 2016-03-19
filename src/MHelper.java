b2309e1c24a1a500de8ef21bf154bebadf0b1d86
Gravituss <zaretskyyy@gmail.com>
2016-03-20 00:55:59 +0200

    btw commit 03

--- src/MHelper.java
+++ src/MHelper.java
@@ -18,17 +18,25 @@ public class MHelper {
 		// initializing WebDriver instance, opening Miniaylo webpage,
 		// waiting for some bottom-located web element appear on the webpage
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://miniaylo.finance.ua");
+		driver.get("http://miniaylo.finance.ua/ru/");
 		WebDriverWait wait = new WebDriverWait(driver, 45);
 		wait.until(ExpectedConditions.visibilityOfElementLocated
 				(By.cssSelector("div.span12.bottomsearch_line")));
 		
+		/*WebDriverWait wait2 = new WebDriverWait(driver, 45);
+		wait2.until(ExpectedConditions.visibilityOfElementLocated
+				(By.cssSelector("input.typeahead.tt-input.selected")));	*/	
+		
 		System.out.println("123");
 		
 		// typing city into the "input region" field
-		WebElement city = driver.findElement(By.cssSelector("input.typeahead.tt-input.selected"));
-		System.out.println("123");
-		city.sendKeys("київ");
+		WebElement city = driver.findElement(By.cssSelector("input.typeahead.tt-input"));
+		//if (city.isDisplayed()){System.out.println("city is here");}
+		city.click();
+		city.clear();
+		
+		
+		city.sendKeys("киев");
 		
 		
 		
