5099bc8e06f3f06952450181b792aad4c58a2f0e
Gravituss <zaretskyyy@gmail.com>
2016-03-19 22:37:36 +0200

    btw commit 02

--- src/MHelper.java
+++ src/MHelper.java
@@ -1,6 +1,5 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,16 +9,26 @@ public class MHelper {
 
 	public static void main(String[] args) {
 		
+		// preparing data for input
+		String currency = "USD";
+		boolean weBuy = true;
+		
+		
+		
 		// initializing WebDriver instance, opening Miniaylo webpage,
 		// waiting for some bottom-located web element appear on the webpage
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://miniaylo.finance.ua");
 		WebDriverWait wait = new WebDriverWait(driver, 45);
 		wait.until(ExpectedConditions.visibilityOfElementLocated
-				(By.cssSelector("div.span12.bottomsearch_line")));		
-		
+				(By.cssSelector("div.span12.bottomsearch_line")));
 		
 		System.out.println("123");
+		
+		// typing city into the "input region" field
+		WebElement city = driver.findElement(By.cssSelector("input.typeahead.tt-input.selected"));
+		System.out.println("123");
+		city.sendKeys("київ");
 		
 		
 		
